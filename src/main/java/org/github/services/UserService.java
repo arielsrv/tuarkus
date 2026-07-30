3d774bcff80f9f2080420058050b26b09d7984f2
arielpineiro <arielsrv@gmail.com>
2026-07-30 11:57:14 -0300

    fixes

--- src/main/java/org/github/services/UserService.java
+++ src/main/java/org/github/services/UserService.java
@@ -4,7 +4,9 @@ import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.github.clients.GoRestClient;
+import org.github.clients.responses.PostResponse;
 import org.github.clients.responses.TodoResponse;
+import org.github.clients.responses.UserResponse;
 import org.github.dto.CommentDTO;
 import org.github.dto.PostDTO;
 import org.github.dto.TodoDTO;
@@ -28,19 +30,28 @@ public class UserService {
                 .onItem().transformToUni(users -> joinAll(users.stream()
                         .map(user -> {
                             // Second level of fan-out: for each user post, its comments in parallel.
-                            Uni<List<PostDTO>> posts = this.client.getPosts(user.id())
-                                    .onItem().transformToUni(userPosts -> joinAll(userPosts.stream()
-                                            .map(post -> this.client.getComments(post.id())
-                                                    .map(comments -> new PostDTO(post.id(), post.title(), comments.stream()
-                                                            .map(comment -> new CommentDTO(comment.id(), comment.name(), comment.email(), comment.body()))
-                                                            .toList())))
-                                            .toList()));
+                            Uni<List<PostDTO>> posts = this.getPosts(user);
                             Uni<List<TodoResponse>> todos = this.client.getTodos(user.id());
+
                             return Uni.combine().all().unis(posts, todos).asTuple()
                                     .map(tuple -> new UserDTO(user.id(), user.name(), user.email(), tuple.getItem1(), tuple.getItem2().stream()
                                             .map(todo -> new TodoDTO(todo.id(), todo.title(), todo.body(), todo.dueOn()))
                                             .toList()));
                         })
+                        .toList()));
+    }
+
+    private Uni<List<PostDTO>> getPosts(UserResponse user) {
+        return this.client.getPosts(user.id())
+                .onItem().transformToUni(userPosts -> joinAll(userPosts.stream()
+                        .map(this::getComments)
+                        .toList()));
+    }
+
+    private Uni<PostDTO> getComments(PostResponse post) {
+        return this.client.getComments(post.id())
+                .map(comments -> new PostDTO(post.id(), post.title(), comments.stream()
+                        .map(comment -> new CommentDTO(comment.id(), comment.name(), comment.email(), comment.body()))
                         .toList()));
     }
 }
